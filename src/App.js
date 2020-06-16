ad844df57b308e573cc0615b605b467ca376d717
Miguel Jesus <miguel.jesus@talkdesk.com>
2020-06-16 14:58:55 +0100

    add debug config for chrome debug

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 12;
+  const b = 11;
+  const sum = a + b;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and hit({sum}) save to reload.
         </p>
         <a
           className="App-link"
